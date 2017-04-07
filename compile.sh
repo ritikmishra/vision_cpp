3465e49aec17b7fadfd5ae743a29a15003580b5e
Ritik Mishra <ritik.mishra314@gmail.com>
2017-04-06 21:40:11 -0500

    Now with NetworkTables!

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-file="$1"
-g++ -o "$file" "$file.cpp" EdgeTest.h EdgeTest.cpp Utils.h Utils.cpp -I/usr/local/include/opencv -I/usr/local/include -L/usr/local/lib -lopencv_shape -lopencv_stitching -lopencv_objdetect -lopencv_superres -lopencv_videostab -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_video -lopencv_photo -lopencv_ml -lopencv_imgproc -lopencv_flann -lopencv_core -std=gnu++11
+file="$1" 
+g++ -o "$file" "$file.cpp" EdgeTest.h EdgeTest.cpp Utils.h Utils.cpp -I/usr/local/include/networktables -I/usr/local/include/opencv -I/usr/local/include -L/usr/local/lib -lntcore -lopencv_shape -lopencv_stitching -lopencv_objdetect -lopencv_superres -lopencv_videostab -lopencv_calib3d -lopencv_features2d -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_video -lopencv_photo -lopencv_ml -lopencv_imgproc -lopencv_flann -lopencv_core -std=gnu++11
